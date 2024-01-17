59201ad30393934d2fa48debce5916345026ab80
CN-Mika <83977863+CN-Mika@users.noreply.github.com>
2024-01-17 17:02:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# FLW2 U2L7: Code Solo
+
+## Coin Flipper
+
+In this app:
+- The user clicks a button and the coin lands on either heads or tails.
+- It keeps track of how many times the coin has landed on each.
+
+### Instructions
+
+USE COMMENTS AS CLUES!
+- You'll find lots of comments in the Flip() component, where you'll want to write specific parts of your code.
+- Feel free to delete the comments when you're finished, keep them up, or change them. Remember, leaving comments is another great practice!
+
+---
+
+1. On lines 50-57, there is an image of a coin that changes from heads to tails depending on the state of the coinSide variable.
+   - The syntax may be unfamiliar, but it's using what's called a "ternary" expression.
+   - Essentially, it's an if/else statement but written slightly differently.
+   - These are easier to use in React when it comes to conditionally returning elements.
+
+---
+
+2. You won't be passing any functions as props in this assignment! Since we're building out everything our Flip() component, we'll have everything we need.
+
+---
+
+Bonus Challenge!
+- Add a button that resets the heads & tails count back to 0 and changes the coin image back to heads.
+- You'll need to build a function and add it as an event handler to the button.
+- Look back to the Counter app for syntax help!
---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import './App.css'
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Coin Flipper 🪙</h1>
+      <Flip />
+      <p>New changes</p>
+    </div>
+  );
+}
+
+export default App;
+
+
+function Flip() {
+  // Declare 3 state variables and state setter functions:
+
+  // 1. The first keeps track of the coin landing on heads.
+  //  - It should be initially set to 0.
+  //  - Add your code here!
+
+  // 2. The next should do the same but with tails.
+  //  - It should also initially be set to 0.
+  //  - Add your code here!
+
+  // 3. Lastly, set the initial state for the coin side.
+  //  - Call the state variable coinSide
+  //  - Add your code here!
+
+  function flipCoin() {
+    // Math.random generates a random number greater than 0 and less than 1.
+    let flip = Math.random();
+    if (flip < 0.5) {
+      flip = "heads";
+      // 4. Increase the heads count every time the coin lands on heads
+      //  - Set coinSide to heads so the coin image changes accordingly
+    } else {
+      flip = "tails";
+      // 5. Increase the tails count every time the coin lands on tails
+      //  - Set coinSide to tails so the coin image changes accordingly.
+    }
+  }
+
+  return (
+    <div className="flip">
+      <div className="coin">
+        {/* Uncomment lines 30-37 when finished! */}
+        {/* <img
+          src={
+            coinSide === "heads"
+              ? "https://www.usmint.gov/wordpress/wp-content/uploads/2020/12/2021-general-george-washington-crossing-the-delaware-quarter-uncirculated-obverse-philadelphia-768x768.jpg"
+              : "https://jeremythemagician.com/wp-content/uploads/2012/11/Quarter_new.jpg"
+          }
+          alt="coin"
+        /> */}
+      </div>
+      {/* 6. Write an event handler to flip the coin!  */}
+      <button>Click to flip!</button>
+      
+      {/* Counts how many times the coin has landed on heads. */}
+      {/* 7. Change 0 to the heads count state variable  */}
+      <p>Heads: {0}</p>
+      
+      {/* Counts how many times the coin has landed on tails */}
+      {/* 8. Change 0 to the tails count state variable  */}
+      <p>Tails: {0}</p>
+    </div>
+  );
+}
